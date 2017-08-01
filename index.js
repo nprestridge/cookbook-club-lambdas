7536f5304c6d3a605cc805d2482f4bd3842881cc
Nancy Prestridge <nancy.the.dev@gmail.com>
2017-07-31 20:36:58 -0400

    feat: Update index.js to use common handler

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 import Config from './src/Config';
 import Controller from './src/Controller';
 
-exports.createCookbook = async (event, context, callback) => {
-  try {
-    const config = Config.load();
-    const controller = new Controller(config);
-    const response = await controller.createCookbook(event);
+const config = Config.load();
+const controller = new Controller(config);
 
+/**
+ * A map of lambda handlers to controller methods
+ * @return {Function} handler function
+ */
+const handlers = {
+  createCookbook: function handler(event) {
+    return controller.createCookbook(event);
+  },
+  deleteCookbook: function handler(event) {
+    return controller.deleteCookbook(event);
+  },
+  getCookbooks: function handler(event) {
+    return controller.getCookbooks(event);
+  },
+  getCookbookRecipes: function handler(event) {
+    return controller.getCookbookRecipes(event);
+  },
+};
+
+/**
+ * Generic handler to execute controller methods
+ * @param  {string}   handlerName   Method name
+ * @param  {Object}   event         JSON event parameters
+ * @param  {Object}   context       Lambda context object
+ * @param  {Function} callback      Callback funcion
+ * @return {Promise}
+ */
+exports.execute = async (handlerName, event, context, callback) => {
+  try {
+    const response = await handlers[handlerName](event);
     return callback(null, response);
   } catch (e) {
-    console.error(`FATAL Caught: ${JSON.stringify(e)}`);
+    console.error(`FATAL Caught: ${JSON.stringify(e)}`); // eslint-disable-line no-console
     return callback(e);
   }
+};
+
+exports.createCookbook = async (event, context, callback) => {
+  exports.execute('createCookbook', event, context, callback);
 };
 
 exports.deleteCookbook = async (event, context, callback) => {
-  try {
-    const config = Config.load();
-    const controller = new Controller(config);
-    const response = await controller.deleteCookbook(event);
-
-    return callback(null, response);
-  } catch (e) {
-    console.error(`FATAL Caught: ${JSON.stringify(e)}`);
-    return callback(e);
-  }
+  exports.execute('deleteCookbook', event, context, callback);
 };
 
 exports.getCookbooks = async (event, context, callback) => {
-  try {
-    const config = Config.load();
-    const controller = new Controller(config);
-    const response = await controller.getCookbooks(event);
-
-    return callback(null, response);
-  } catch (e) {
-    console.error(`FATAL Caught: ${JSON.stringify(e)}`);
-    return callback(e);
-  }
+  exports.execute('getCookbooks', event, context, callback);
 };
 
 exports.getCookbookRecipes = async (event, context, callback) => {
-  try {
-    const config = Config.load();
-    const controller = new Controller(config);
-    const response = await controller.getCookbookRecipes(event);
-
-    return callback(null, response);
-  } catch (e) {
-    console.error(`FATAL Caught: ${JSON.stringify(e)}`);
-    return callback(e);
-  }
+  exports.execute('getCookbookRecipes', event, context, callback);
 };
